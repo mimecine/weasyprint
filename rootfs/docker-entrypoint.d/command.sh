34e380966934c5577156fe3906eaf210088c276a
Marco <marco@mimecom.net>
2020-02-26 17:26:26 -0500

    Trying to add to-host networking.

--- rootfs/docker-entrypoint.d/command.sh
+++ rootfs/docker-entrypoint.d/command.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+HOST_DOMAIN="host.docker.internal"
+ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
+  echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
+fi
+echo -e "$HOST_IP\twp.test" >> /etc/hosts
+
+
 if [ "${1:0:1}" = '-' ] || [ -f "$1" -a ! -x "$1" ] || [ -n "$(wget -S --spider -O/dev/null "$1" 2>&1 | grep 'HTTP')" ]; then
     set -- weasyprint "$@"
 fi
